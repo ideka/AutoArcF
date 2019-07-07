b1072a97a0a111a0cd18a6ff173c2e4fe954b5e9
Gerardo Marset <gammer1994@gmail.com>
2019-07-07 10:25:14 -0300

    Clean up and reorganize code

--- Program.fs
+++ Program.fs
@@ -9,95 +9,100 @@ let localBakPath = "bin64/bak/"
 let gameExeName = "Gw2-64.exe"
 
 type Remote = {
-    name:string;
-
-    remotePath:string;
-    checksumPath:string option;
-    localName:string option;
+    name: string
+    remotePath: string
+    checksumPath: string option
+    overrideName: string option
 }
+with
+    member remote.localName =
+        match remote.overrideName with
+        | Some value -> value
+        | None -> remote.remotePath.Split([|'/'|]) |> Array.last
 
-let localNameOf (remote:Remote) =
-    match remote.localName with
-    | Some(value) -> value
-    | None -> remote.remotePath.Split [|'/'|] |> Array.last
+    member remote.fullLocalPath = localPath + remote.localName
+    member remote.fullBakPath = localBakPath + remote.localName
+end
 
-let updateRemote (remote:Remote) =
-    let fullLocalPath = localPath + localNameOf remote
-
-    let updateExists =
+module Remote = 
+    let updateExists (remote: Remote) =
         printfn "Checking for %s updates..." remote.name
-        if not <| File.Exists(fullLocalPath) then
+        if not <| File.Exists(remote.fullLocalPath) then
             printfn "Local file not found, downloading..."
             true
         else
-            use resp = WebRequest.Create(remote.remotePath).GetResponse():?>HttpWebResponse
+            use resp = WebRequest.Create(remote.remotePath).GetResponse() :?> HttpWebResponse
             let lastModifiedRepr = resp.LastModified.ToString()
-            if File.GetLastWriteTimeUtc(fullLocalPath) >= resp.LastModified then
+            if File.GetLastWriteTimeUtc(remote.fullLocalPath) >= resp.LastModified then
                 printfn "Up to date (%s)." lastModifiedRepr
                 false
             else
                 printfn "Downloading update (%s)..." lastModifiedRepr
                 Directory.CreateDirectory(localBakPath) |> ignore
-                File.Copy(fullLocalPath, localBakPath + localNameOf remote, true)
+                File.Copy(remote.fullLocalPath, remote.fullBakPath, true)
                 true
 
-    if updateExists then
+    let tryDownload (remote: Remote) =
         use wc = new WebClient()
         let file = wc.DownloadData(remote.remotePath)
+        match remote.checksumPath with
+        | None ->
+            printfn "No checksum verification for %s." remote.name
+            Some file
+        | Some checksumPath ->
+            use md5 = MD5.Create()
+            let checksum = wc.DownloadString(checksumPath).Split([|' '|]) |> Array.head |> String.filter (not << Char.IsWhiteSpace)
+            let fileChecksum = file |> md5.ComputeHash |> BitConverter.ToString |> String.filter (fun c -> c <> '-') |> String.map Char.ToLower
+            let equal = checksum = fileChecksum
+            printfn "%s == %s (%b)" checksum fileChecksum equal
+            if not <| equal then
+                Console.ReadKey() |> ignore
+                None
+            else
+                Some file
 
-        let checksumVerified =
-            match remote.checksumPath with
-            | None ->
-                printfn "No checksum verification for %s." remote.name
-                true
-            | Some(checksumPath) ->
-                use md5 = MD5.Create()
-                let checksum = (wc.DownloadString(checksumPath).Split [|' '|] |> Array.head).Trim()
-                let downloadedChecksum = BitConverter.ToString(md5.ComputeHash(file)).Replace("-", "").ToLower()
-                let equal = checksum = downloadedChecksum
-                printfn "%s == %s (%b)" checksum downloadedChecksum equal
-                if not <| equal then
-                    Console.ReadKey() |> ignore
-                equal
+    let update (remote: Remote) =
+        if updateExists remote then
+            match tryDownload remote with
+            | None -> ()
+            | Some file ->
+                File.WriteAllBytes(remote.fullLocalPath, file)
 
-        if checksumVerified then
-            File.WriteAllBytes(fullLocalPath, file)
-
-    printfn ""
+        printfn ""
 
 [<EntryPoint>]
 let main argv =
-    updateRemote {
-        name = "ArcDPS";
-        remotePath = "https://www.deltaconnected.com/arcdps/x64/d3d9.dll";
-        checksumPath = Some("https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum");
-        localName = None;
+    Remote.update {
+        name = "ArcDPS"
+        remotePath = "https://www.deltaconnected.com/arcdps/x64/d3d9.dll"
+        checksumPath = Some "https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum"
+        overrideName = None
     }
 
-    updateRemote {
-        name = "ArcDPS Build Templates";
-        remotePath = "https://www.deltaconnected.com/arcdps/x64/buildtemplates/d3d9_arcdps_buildtemplates.dll";
-        checksumPath = None;
-        localName = None;
+    Remote.update {
+        name = "ArcDPS Build Templates"
+        remotePath = "https://www.deltaconnected.com/arcdps/x64/buildtemplates/d3d9_arcdps_buildtemplates.dll"
+        checksumPath = None
+        overrideName = None
     }
 
-    updateRemote {
-        name = "ArcDPS Extras";
-        remotePath = "https://www.deltaconnected.com/arcdps/x64/extras/d3d9_arcdps_extras.dll";
-        checksumPath = None;
-        localName = None;
+    Remote.update {
+        name = "ArcDPS Extras"
+        remotePath = "https://www.deltaconnected.com/arcdps/x64/extras/d3d9_arcdps_extras.dll"
+        checksumPath = None
+        overrideName = None
     }
 
-    updateRemote {
-        name = "ArcDPS Mechanics";
-        remotePath = "http://martionlabs.com/wp-content/uploads/d3d9_arcdps_mechanics.dll";
-        checksumPath = Some("http://martionlabs.com/wp-content/uploads/d3d9_arcdps_mechanics.dll.md5sum");
-        localName = None;
+    Remote.update {
+        name = "ArcDPS Mechanics"
+        remotePath = "http://martionlabs.com/wp-content/uploads/d3d9_arcdps_mechanics.dll"
+        checksumPath = Some "http://martionlabs.com/wp-content/uploads/d3d9_arcdps_mechanics.dll.md5sum"
+        overrideName = None
     }
 
     match Array.tryLast argv with
     | None -> Process.Start(gameExeName)
-    | Some(arg) -> Process.Start(gameExeName, arg)
+    | Some arg -> Process.Start(gameExeName, arg)
     |> ignore
 
     0
